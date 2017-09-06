d9aa14b8e48c1ce134527ed1c296d7d16868ed0b
topaz <1634940259@qq.com>
2017-09-06 14:36:13 +0800

    删除了dept的相关代码

---
+++ scm32/src/cn/itcast/scm/dao/AccountMapper.java
@@ -0,0 +1,8 @@
+package cn.itcast.scm.dao;
+
+import cn.itcast.scm.entity.Account;
+import java.util.List;
+
+public interface AccountMapper {
+
+}
---
+++ scm32/src/cn/itcast/scm/dao/SupplierMapper.java
@@ -0,0 +1,8 @@
+package cn.itcast.scm.dao;
+
+import cn.itcast.scm.entity.Supplier;
+import java.util.List;
+
+public interface SupplierMapper {
+   public int insertSupplier(Supplier supplier);
+}
